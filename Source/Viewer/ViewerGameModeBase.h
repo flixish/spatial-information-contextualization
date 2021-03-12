19314d391e2b439a2736bb5cb5af7722a25ea188
Felix Hüning <mail@felixhuening.de>
2021-03-12 19:48:18 +0100

    first commit

---
+++ Source/Viewer/ViewerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ViewerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIEWER_API AViewerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
